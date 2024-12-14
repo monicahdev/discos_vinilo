9c2b27c6176958ca3ffae39e057515451ccc9fa1
Monica Hernandez <monicahdev@gmail.com>
2024-12-14 23:12:16 +0100

    added menu.php

---
+++ menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <nav class="menu">
+        <a href="index.php">Home</a>
+        <a href="random-post.php">Disco</a>
+        <a href="records.php">Discos</a>
+        <a href="api/records/1" target="_blank">API_discos</a>
+        <a href="api/record/1" target="_blank">API_disco</a>
+        <a href="login.php">Login</a>
+        <a href="signup.php">Sign up</a>
+        <a href="edit.php">Perfil de usuario</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+</body>
+</html>
