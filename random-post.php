503a4cb8ec6b7821f35ee1db5e57f2b4ae884b18
Monica Hernandez <monicahdev@gmail.com>
2024-12-14 14:28:23 +0100

    tweaked styles.css and random-post.php

--- random-post.php
+++ random-post.php
@@ -5,26 +5,27 @@ require 'db_config.php';
 try {
     //Consultar en la tabla y mostrar 1 solo disco
     $query = "SELECT * FROM discosvinilo ORDER BY RAND() LIMIT 1";
-    $stmt = $library_pdo->query($query);
+    $statement = $library_pdo->query($query);
 
-    //Mostrar el disco
-    $vinyl_record = $stmt->fetch(PDO::FETCH_ASSOC);
+    //Traer el disco de la consulta
+    $vinyl_record = $statement->fetch(PDO::FETCH_ASSOC);
 
     /*Asegurarse de que existe el disco y mostrarlo con estas características*/
     if ($vinyl_record) {
         
         echo "<h1>Discos de vinilo</h1>";
-        echo "<p>Título del disco: " . htmlspecialchars($vinyl_record['titulo_album']) . "</p>";
-        echo "<p>Artista: " . htmlspecialchars($vinyl_record['artista']) . "</p>";
-        echo "<p>Género musical: " . htmlspecialchars($vinyl_record['genero_musical']) . "</p>";
-        echo "<p>Año de lanzamiento: " . htmlspecialchars($vinyl_record['lanzamiento']) . "</p>";
-        echo "<p>Estado del disco: " . htmlspecialchars($vinyl_record['estado_disco']) . "</p>";
-        echo "<p>Precio: €" . htmlspecialchars($vinyl_record['precio']) . "</p>";
-        echo "<p>Imagen de portada:</p><img src='" . htmlspecialchars($vinyl_record['imagen_portada']) . "' alt='portada';'>";
+        echo "<p><strong>Título del disco: </strong>" . htmlentities($vinyl_record['titulo_album']) . "</p>";
+        echo "<p><strong>Artista: </strong>" . htmlentities($vinyl_record['artista']) . "</p>";
+        echo "<p><strong>Género musical: </strong>" . htmlentities($vinyl_record['genero_musical']) . "</p>";
+        echo "<p><strong>Año de lanzamiento: </strong>" . htmlentities($vinyl_record['lanzamiento']) . "</p>";
+        echo "<p><strong>Estado del disco: </strong>" . htmlentities($vinyl_record['estado_disco']) . "</p>";
+        echo "<p><strong>Precio: € </strong>" . htmlentities($vinyl_record['precio']) . "</p>";
+        echo "<p><strong>Imagen de portada: </strong></p><img src='" . htmlentities($vinyl_record['imagen_portada']) . "' alt='Portada del vinilo';'>";
     } else {
         echo "<p>No hay discos en la base de datos.</p>";
     }
 } catch (PDOException $e) {
-    echo "Error al obtener el disco: " . $e->getMessage(); //Mensaje en caso de error
+    //Mensaje en caso de error
+    echo "Error al obtener el disco: " . $e->getMessage(); 
 }
 ?>
