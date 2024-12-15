28c237c0b2bf06107994482c4048271baaab704b
Monica Hernandez <monicahdev@gmail.com>
2024-12-15 18:03:58 +0100

    modified html in index.php

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<?php
-// Importar credenciales de la configuración de la base de datos
-require 'db_config.php';
-
-try {
-    // Consulta para obtener los 2 discos fijos
-    $query_fixed_records = "SELECT id, titulo_album, artista, precio, imagen_portada FROM discosvinilo WHERE id IN (5, 6)";
-    $fixed_statement = $library_pdo->query($query_fixed_records);
-    $fixed_records = $fixed_statement->fetchAll(PDO::FETCH_ASSOC);
-
-    // Consulta para obtener 3 discos aleatorios
-    $query_random_records = "SELECT id, titulo_album, artista, precio, imagen_portada FROM discosvinilo WHERE id NOT IN (5, 6) ORDER BY RAND() LIMIT 3";
-    $random_statement = $library_pdo->query($query_random_records);
-    $random_records = $random_statement->fetchAll(PDO::FETCH_ASSOC);
-
-    // Array para guardar los discos fijos y aleatorios a mostrar
-    $all_vinyls = array_merge($fixed_records, $random_records);
-
-} catch (PDOException $e) {
-    die("Error al obtener los discos: " . $e->getMessage());
-}
-?>
-
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -30,7 +7,29 @@ try {
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        
+        <?php
+        // Importar credenciales de la configuración de la base de datos
+        require 'db_config.php';
+
+        try {
+            // Consulta para obtener los 2 discos fijos
+            $query_fixed_records = "SELECT id, titulo_album, artista, precio, imagen_portada FROM discosvinilo WHERE id IN (5, 6)";
+            $fixed_statement = $library_pdo->query($query_fixed_records);
+            $fixed_records = $fixed_statement->fetchAll(PDO::FETCH_ASSOC);
+
+            // Consulta para obtener 3 discos aleatorios
+            $query_random_records = "SELECT id, titulo_album, artista, precio, imagen_portada FROM discosvinilo WHERE id NOT IN (5, 6) ORDER BY RAND() LIMIT 3";
+            $random_statement = $library_pdo->query($query_random_records);
+            $random_records = $random_statement->fetchAll(PDO::FETCH_ASSOC);
+
+            // Array para guardar los discos fijos y aleatorios a mostrar
+            $all_vinyls = array_merge($fixed_records, $random_records);
+
+        } catch (PDOException $e) {
+            die("Error al obtener los discos: " . $e->getMessage());
+        }
+        ?>    
+
         <h1>Tienda de vinilos</h1>
         <?php include 'menu.php'; ?>
         <div class="container">
